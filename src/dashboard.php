68d02c31e7fb2f6b3d0b54a06e6d71baa450f109
KimYe9 <70550084+KimYe9@users.noreply.github.com>
2024-04-07 23:29:17 -0400

    nav bar created

--- src/dashboard.php
+++ src/dashboard.php
@@ -18,19 +18,25 @@ if (!isset($_SESSION['user_id'])) {
     <link href="frontend/css/style.css" rel="stylesheet">
 </head>
 <body>
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link float-end" href="#">Link</a>
-        </li>
-        <a href="logout.php" class="btn btn-danger d-grid gap-2 d-md-flex justify-content-md-end">Logout</a>
-    </ul>
     <div class="container">
+        <ul class="nav nav-tabs">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home Page</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">RSOs</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link float-end" href="#">Create RSO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link float-end" href="#">Events</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link float-end" href="#">Create Events</a>
+            </li>
+            <a href="logout.php" class="btn btn-danger d-grid gap-2 d-md-flex justify-content-md-end">Logout</a>
+        </ul>
         <div class="row mt-3">
             <div class="col-md-12">
                 <h1>Welcome <?php echo $_SESSION['user_fullname']; ?>!</h1>
